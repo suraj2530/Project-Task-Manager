4004a435aabfbaa6c6799f7eadd1b01daeac986f
Suraj Gupta <38837526+suraj2530@users.noreply.github.com>
2021-10-23 19:27:50 +0530

    Update urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,9 +5,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('task.api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+   #path('api-auth/', include('rest_framework.urls')),
 
-    # path('accounts/', include('user_app.api.urls')) 
+    path('accounts/', include('user_app.api.urls')) 
 
 
 ]
